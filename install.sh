9c1c370fb97faed67279b254ed46e6534f2fdd47
patrik nilsson <patrik.nilsson@orestad-linux.se>
2015-03-31 16:04:08 +0200

    This wasn't meant to be.

--- install.sh
+++
@@ -1,1 +0,0 @@
-apt-get purge libevent-dev, libssl-dev, libgcrypt11-dev, libkrb5-dev, libpam0g-dev, libwrap0-dev, libdb-dev, libtdb-dev, libmysqlclient-dev, libavahi-client-dev, libacl1-dev, libldap2-dev, libcrack2-dev, systemtap-sdt-dev, libdbus-1-dev, libdbus-glib-1-dev, libglib2.0-dev, tracker, libtracker-sparql-0.14-dev, libtracker-miner-0.14-dev
